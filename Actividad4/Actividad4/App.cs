bdc86c8290b0d37a302423f58e3063a37aed5032
narno67@comunidad.unam.mx <onran67>
2015-09-16 14:13:29 -0500

    * App.cs: se cambio a Inicializador de Objetos
    * repositories.config:
    * Resource.designer.cs:

--- Actividad4/Actividad4/App.cs
+++ Actividad4/Actividad4/App.cs
@@ -7,20 +7,17 @@ namespace Actividad4
 	{
 		public static Page GetMainPage ()
 		{	
-			//Se crea un objeto etiqueta
-			Label label = new Label();
-
-			//Se modifican sus propiedades
-			//Texto
-			label.Text = "Cámbiame";
-			//Color del texto
-			label.TextColor = Color.Blue;
-			//Alineación vertical (YAlign). Para la alineación horizontal, usar XAlign
-			label.YAlign = TextAlignment.Center;
-
-
-			//El mismo código usando inicializador de objetos
-			//Label label = new Label{ Text = "Cámbiame", TextColor =  Color.Blue, YAlign = TextAlignment.Center };
+			//Se crea un objeto etiqueta usando Inicializador de Objetos
+			Label label = new Label {
+			//Se le asigna su valor, en este caso el texto Cámbiame
+				Text = "Cámbiame",			
+			//Se le cambia el Color al texto seleccionamos color verde
+			TextColor = Color.Green,
+			//Se alinea el texto verticalmente (YAlign)
+				YAlign = TextAlignment.Center,
+			//Se alinea el texto horizontalmente (XAlign)
+			XAlign = TextAlignment.Center };
+			
 
 			//Se crea una página y se le asigna como contenido la eqtiqueta que se creó
 			ContentPage contentPage = new ContentPage();
