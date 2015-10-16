ee12dd3cdb0a9babc77583f0cd58bf27d3d8dd54
narno67@comunidad.unam.mx <onran67>
2015-10-15 23:32:15 -0500

    Complete el codigo

--- Actividad8/Actividad8/Contenido.cs
+++ Actividad8/Actividad8/Contenido.cs
@@ -7,7 +7,17 @@ namespace Actividad8
 	{
 		public Contenido ()
 		{
-
+			Label texto = new Label {
+				Text = "Usuario y Pasword Validos",
+				TextColor = Color.Green
+			};
+			StackLayout stackLayout = new StackLayout {
+				Children = {
+					texto
+				}
+			};
+			this.Content = stackLayout;
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
 		}
 	}
 }
