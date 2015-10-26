4f3c4a8ef60a7d43f09c1dce08e7195e5db57158
narno67@comunidad.unam.mx <onran67>
2015-10-26 14:10:34 -0600

    * App.cs: * Login.cs: * repositories.config: * Resource.designer.cs:
    * Actividad10.projitems: Agregue el Login

---
+++ Actividad10/Actividad10/Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Actividad10
+{
+	public class Login: ContentPage
+	{
+		public Login ()
+		{
+			Entry usuario = new Entry { Placeholder = "Usuario" };
+			Entry clave = new Entry { Placeholder = "Clave", IsPassword = true };
+
+			Button boton = new Button {
+				Text = "Login",
+				TextColor = Color.White,
+				BackgroundColor = Color.FromHex ("77D065") 
+			};
+
+			boton.Clicked += (sender, e) => {
+
+				if (usuario.Text.Equals ("edgar") && (clave.Text.Equals ("zamora"))) {
+					Navigation.PushAsync (new Contenido ());
+				}
+			}; 
+
+			//Stacklayout permite apilar los controles verticalmente
+			StackLayout stackLayout = new StackLayout 
+			{
+				Spacing = 20, 
+				Padding = 50,
+				VerticalOptions = LayoutOptions.Center,
+				Children = 
+				{
+					usuario,
+					clave,
+					boton
+				}
+				};
+
+			//Como esta clase hereda de ContentPage, podemos usar estas propiedades directamente
+			this.Content = stackLayout;
+			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
+		}
+	}
+}
