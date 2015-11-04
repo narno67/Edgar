9b3a4a75d1b2b80c0c7a240e651f987535f362d1
narno67@comunidad.unam.mx <onran67>
2015-11-03 22:40:52 -0600

    * Contenido.cs: Agregue tres frutas
    * repositories.config:
    * Resource.designer.cs:

--- Actividad11/Actividad11/Contenido.cs
+++ Actividad11/Actividad11/Contenido.cs
@@ -33,6 +33,10 @@ namespace Actividad11
 
 			lista.Add (new Frutas{Nombre = "Platano", Imagen = "http://www.importancia.org/wp-content/uploads/platano.jpg"});
 			lista.Add (new Frutas{Nombre = "Pina", Imagen = "http://2.bp.blogspot.com/-Bw0n6k40hD0/T-yH2CGcHyI/AAAAAAAAAI8/kZEiZ_sbq2w/s1600/Pi%C3%B1a_con_hojaStevia.jpg"});
+			lista.Add (new Frutas{Nombre = "Fresa", Imagen = "http://ecocosas.com/wp-content/uploads/2012/10/img_cuales_son_los_beneficios_de_las_fresas_7561_orig.jpg" });
+			lista.Add (new Frutas{Nombre = "Sandia", Imagen = "http://quientieneelpoder.com/wp-content/uploads/2014/09/cocina-sana_recetas-1_grande.jpg" });
+			lista.Add (new Frutas{Nombre = "Kiwi", Imagen = "http://tshop.r10s.com/31a71c40-787c-11e3-ac10-005056b72f9f/20140118/70ce50ca-0c1a-43ce-9531-6a16fda6fb5a.jpg" });
+
 
 
 			return lista;
